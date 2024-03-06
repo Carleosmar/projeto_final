dc43e5cec76caca9c0a001d81a9f56e7380a70c8
carleosmar <karleosmar@hotmail.com>
2024-03-05 21:53:55 -0300

    error de metodo indefinido

--- public/index.php
+++ public/index.php
@@ -3,10 +3,12 @@ require_once(dirname(__FILE__, 2) . '/src/config/config.php');
 require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
 $user = new User(['name' => 'Lucas', 'email' => 'lucas@cod3r.com.br']);
-print_r($user);
-echo '<br><br>';
-$user->email = 'lucas_alterado@cod3r.com.br';
-print_r($user->email);
+
+
+print_r(User::get(['id' => 1], 'name, email'));
+
+
+//echo $user->getSelect();
 
 /**************************
 $sql = 'select * from users';
